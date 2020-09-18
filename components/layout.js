16775e0583671a96ea5145b211c6566855e38dd8
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-09-18 12:44:23 -0400

    Removed powered by Vercel a tag

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Nathan Page'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Nathan Page'
 
 export default function Layout({ children, home }) {
   return (
@@ -70,14 +70,6 @@ export default function Layout({ children, home }) {
       )}
       <footer className={styles.footer}>
         <p>Copyright &copy; 2020 Nathan Page</p>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
       </footer>
     </div>
   )
