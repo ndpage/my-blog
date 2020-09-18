1da072e062b11b9156853fcaeeda3a347ba824ad
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-09-18 15:16:37 -0400

    Removed siteTitle2 from index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Layout, { siteTitle, siteTitle2 } from '../components/layout'
+import Layout, { siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle2}</title>
+        <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <h2>Intro</h2>
