c39743824c2c1c3f691a6b7704c892adf562d3e8
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-09-18 11:23:57 -0400

    Added comments to clarify css module usage

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,14 @@
+
+
+/* 
+  CSS modules are only used for component styling. 
+  To use global CSS, change the code in global.css
+*/
+
+import styles from './layout.module.css' 
+
 function Layout({ children }) {
-  return <div>{children}</div>
+  return <div className={styles.container}>{children}</div>
 }
 
 export default Layout
