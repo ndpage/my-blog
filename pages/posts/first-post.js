68cf1cb4a32ad1c393d318aaceabd760f9104587
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-09-18 11:54:41 -0400

    Added Layout component and lorem text

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,20 @@
-import Link from 'next/link'
+
+import Head from 'next/head'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
     <>
+    <Head>  
+      <title>First Post</title>
+    </Head>
+    <Layout>
       <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. 
+        Voluptatum ducimus voluptates voluptas?</p>
+      <p>Thanks for reading!</p>
+      </Layout>
     </>
   )
 }
