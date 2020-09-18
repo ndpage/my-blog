c503df4c7fbc1148c35d3198f08662f85f5a16df
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-09-18 15:19:17 -0400

    Moved caption text to bottom of post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -15,9 +15,8 @@ export default function FirstPost() {
   <title>{pageName}</title>
     </Head>
       <Layout>
-          <h1>{pageName}</h1>
-          <p className={UtilsStyle.subtext}>Semptember 18, 2020</p> 
         <Post> 
+          <h1>{pageName}</h1>
           <p>Hello world!</p> 
           <p> As an engineer and tech enthusiast, I was wanted to find a way to connect with other engineers and individuals who are passionate about technology, so I created this website
                to share my thoughts and connect with you! 
@@ -26,6 +25,7 @@ export default function FirstPost() {
           <p>I am an engineer at <a href="https://www.boschrexroth.com/en/xc/" target="_blank">Bosch Rexroth</a> in the Greeenville, South Carolina area.I am part of the Mobile Hydraulics Research and Devleopment group, with a focus on mechatronics systems.</p>
           <p> I graduated May 2018 from <a href="https://www.ncsu.edu/" target="_blank"> North Carolina State University</a> with a Bachelor of Science in Engineering with a concentration in Mechatronics (electro-mechanical systems).</p>
           <p>Thanks for reading!</p>
+          <p className={UtilsStyle.subtext}>Written Semptember 18, 2020</p> 
         </Post>
       </Layout>
     </>
