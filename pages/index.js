279ad06c706786ec4d1e7e043c5d2fdd45e65643
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-09-18 15:14:55 -0400

    Modified text, added social links, and changed link to blog post

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,31 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Layout, { siteTitle } from '../components/layout'
+import Layout, { siteTitle, siteTitle2 } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>{siteTitle2}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello! I am a control systems and software engineer. 
-          Feel free to check out my <a href="www.linkedin.com">LinkedIn</a> page and GitHub page</p>
+        <h2>Intro</h2>
+        <p>Hello! I am a control systems and software engineer based in Greenville, South Carolina. 
+            Feel free to check out my <a href="https://www.linkedin.com/in/nathan-page-2018/">LinkedIn</a> and <a href="https://github.com/ndpage">GitHub</a> pages to see my projects and experience.
+          </p>
+          <p> Check out my blog posts below to learn more about me and my interests and passions.</p>
+      </section>
+      <section className={utilStyles.headingMd}> 
+        <h2> 
+          Blog
+        </h2>
         <p>
-          <Link href="/posts/first-post"><a>My first post</a></Link>
+          <Link href="/posts/first-post">
+            <a>Welcome!
+              <p className={utilStyles.subtext}>Semptember 18, 2020</p> 
+            </a>
+            </Link>
         </p>
       </section>
     </Layout>
