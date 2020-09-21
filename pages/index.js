dfc8ba2c78a56f404f8626a7374ab4725a2e6033
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-09-21 17:24:52 -0400

    Changed intro section tag to an article tag

--- pages/index.js
+++ pages/index.js
@@ -25,14 +25,14 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingSm}>
-        <h2>Intro</h2>
+      <article>
+        <h2 className={utilStyles.headingSm}>Intro</h2>
         <p>Hello! I am a control systems and software engineer based in Greenville, South Carolina. 
             Feel free to check out my <a href="https://www.linkedin.com/in/nathan-page-2018/" target="_blank" rel="noopener noreferrer">LinkedIn </a> 
               and <a href="https://github.com/ndpage" target="_blank" rel="noopener noreferrer"> GitHub</a> pages to see my projects and experience.
           </p>
           <p> Check out my blog posts below to learn more about me and my interests and passions.</p>
-      </section>
+      </article>
       <section> 
       <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
