1e5b1e5b794719e2f39994ea6244735984360871
Nathan Page <33336363+ndpage@users.noreply.github.com>
2021-02-02 21:33:24 -0500

    Put bio query directy in client.fetch( ) within  getStaticProps()

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 //import { getSortedPostsDataFS } from '../lib/posts' //This parses the posts .md files
 //import Date from '../components/date'
 
-import groq from 'groq'
-import client from '../client'
+// Imports for Sanity components and features
+import BlockContent from "@sanity/block-content-to-react";
+import groq from "groq";
+import client from "../client";
 
 const query = groq`
 *[_type == "post" && publishedAt < now()]|order(publishedAt desc)
-`
+`;
+/* const bio = groq`
+*[_type == "bio"][0]
+`; */
+
 /*
   getStaticProps function is used to render a page with data.
   This index.js page uses static side generation
@@ -24,64 +30,67 @@ const query = groq`
     }
   }
   */
-  
-export async function getStaticProps(){
-  const allPostsData = await client.fetch(query)
+
+export async function getStaticProps() {
+  const allPostsData = await client.fetch(query);
+  const bioData = await client.fetch(`*[_type == "bio"][0]`);
   return {
     props: {
       allPostsData,
+      bioData,
     },
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
     // - At most once every second
     revalidate: 5, // In seconds
-  }
+  };
 }
 
-
 /**
- * Function: Home({allPostsData}) 
+ * Function: Home({allPostsData})
  * Description: The Home function houses the short bio and the links to the blog posts
- *               Blog posts are parsed by getSortedPostsDataFS() and displayed with getStaticProps() 
+ *               Blog posts are parsed by getSortedPostsDataFS() and displayed with getStaticProps()
  */
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData, bioData }) {
   return (
     <>
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <article>
-        <p>
-          Control systems and software engineer based in Greenville, South Carolina. I love to learn new technologies and help others learn too. 
-        </p>
-        <p> 
-            Check out my blog posts below to learn more about me and my interests
-        </p>
-        <Link href="/author/nathan-page">
-          <a>
-            About Me
-          </a>
-        </Link>
-      </article>
-      <section>
-      <h2 className={utilStyles.headingLg}>Blog</h2>
-      <ul className={utilStyles.list}>
-        {allPostsData.map(
-              ({ _id, title = '', slug = '', publishedAt = '' }) =>
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <section>
+          <p>
+            <BlockContent blocks={bioData.bio} {...client.config()} />
+          </p>
+          <Link href="/author/nathan-page">
+            <a>
+              <h3>About Me</h3>
+            </a>
+          </Link>
+        </section>
+        <section>
+          <Link href="">
+            <a>
+              <h3>Blog</h3>
+            </a>
+          </Link>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(
+              ({ _id, title = "", slug = "", publishedAt = "" }) =>
                 slug && (
                   <Link href="/post/[slug]" as={`/post/${slug.current}`}>
-                    <a> 
+                    <a>
                       <li key={_id} className={utilStyles.listItem}>
-                        {title}{'  '}({new Date(publishedAt).toDateString()})
-                     </li>
-                   </a>
-                </Link>
+                        {title}
+                        {"  "}({new Date(publishedAt).toDateString()})
+                      </li>
+                    </a>
+                  </Link>
                 )
-              )}
-        </ul>
-      </section>
-    </Layout>
+            )}
+          </ul>
+        </section>
+      </Layout>
     </>
-  )
+  );
 }
