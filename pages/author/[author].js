004621d34f63882ba6dbff841b07be81f14c36ca
Nathan Page <33336363+ndpage@users.noreply.github.com>
2021-02-02 21:32:37 -0500

    Removed unsued imports and added comments above Sanity imports

--- pages/author/[author].js
+++ pages/author/[author].js
@@ -1,101 +1,82 @@
-
 // NextJS components
-import Head from 'next/head'
-import Layout from '../../components/layout'
+import Head from "next/head";
+import Layout from "../../components/layout";
 
-//import { getAllPostIds, getPostData, getSortedPostsDataAPI } from '../../lib/posts'
-//import Date from '../../components/date'
 //import utilStyles from '../../styles/utils.module.css'
 
-import groq from 'groq' //groq is used to query the data 
-import client from '../../client'  // client is a component that connects to the Sanity API with the dataset and ProjectID
-import imageUrlBuilder from '@sanity/image-url'
-import BlockContent from '@sanity/block-content-to-react'
+// Sanity components
+import groq from "groq"; //groq is used to query the data
+import client from "../../client"; // client is a component that connects to the Sanity API with the dataset and ProjectID
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
 
 export const query = groq`*[_type == "author" && slug.current == $slug][0]{
   bio,
   image,
   name
-}`
+}`;
 
 /* 
 Function: urlFor 
 */
-function urlFor (source) {
-  return imageUrlBuilder(client).image(source)
+function urlFor(source) {
+  return imageUrlBuilder(client).image(source);
 }
 
+// Fetch data paths from Sanity API
 export async function getStaticPaths() {
-  console.log("------ getStaticPaths function start ----------")
-  // {'slug':slug.current} was included when using the for loop
-
   const query = `*[_type == "author"]{
     slug
-  }`;  
-  const authors = await client.fetch(query)
-  
-const paths = authors.map(author =>`/author/${author.slug.current}`)
- // Get the paths we want to pre-render based on authors
- //const paths = authors.map((author) => ({
-//  params: {slug: author.slug.current.toString()},
-//}))
-console.log(paths)
-return { paths, fallback: false }
+  }`;
+  const authors = await client.fetch(query);
+
+  // Get the paths we want to pre-render based on authors
+  const paths = authors.map((author) => `/author/${author.slug.current}`);
+
+  return { paths, fallback: false };
 }
 
 // Gets the data from Sanity studio using a GROQ query
-export async function getStaticProps({params}) {
-  console.log("------ getStaticProps function start ----------")
-  console.log(params)
-  const slug = params.author
-  const author = await client.fetch(query, {slug})
+export async function getStaticProps({ params }) {
+  console.log("------ getStaticProps function start ----------");
+  console.log(params);
+  const slug = params.author;
+  const author = await client.fetch(query, { slug });
   return {
     props: {
-        author
-      },
+      author,
+    },
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
     // - At most once every second
     revalidate: 5, // In seconds
-  }
+  };
 }
 
 /*  --------------------------------------------------------------
   Funtion: Post()
   Description: Post function is used to display the fetched data from the Sanity API and GROQ query on the page
 */
-export default function Author({author}){
-  const {
-    name = 'Missing Name',
-    image,
-    bio = []
-  } = author
+export default function Author({ author }) {
+  const { name = "Missing Name", image, bio = [] } = author;
   return (
-    <Layout author> 
-     <Head>
-      <title>{name}</title>
-    </Head>
-    <article>
-      <h1>{name}</h1>
-      { image && (
-        <div>
-          <img
-            src={urlFor(image)
-              .width(300)
-              .url()}
-              />
-        </div>
+    <Layout author>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <article>
+        <h1>{name}</h1>
+        {image && (
+          <div>
+            <img src={urlFor(image).width(300).url()} />
+          </div>
         )}
-      <div>
-      </div>
-      <BlockContent
-        blocks={bio}
-        imageOptions={{ w: 540, h: 460, fit: 'max' }}
-        {...client.config()}
+        <BlockContent
+          blocks={bio}
+          imageOptions={{ w: 540, h: 460, fit: "max" }}
+          {...client.config()}
         />
-    </article>
-  </Layout> 
-  )
+      </article>
+    </Layout>
+  );
 }
-
-
